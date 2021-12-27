f03e18d548beb54a81b7dec4c1a31fac2bf40b54
alanjmrt94 <alanjmartinez94@gmail.com>
2021-12-27 04:11:07 -0300

    Add const file

---
+++ utils/const.py
@@ -0,0 +1,16 @@
+from enum import Enum, unique
+
+
+class Display(Enum):
+    X11 = 'X11Display'
+    WAYLAND = 'GdkWaylandDisplay'
+    WIN32 = 'GdkWin32Display'
+    QUARTZ = 'GdkQuartzDisplay'
+
+
+@unique
+class IdleState(Enum):
+    UNKNOWN = 'unknown'
+    XA = 'xa'
+    AWAY = 'away'
+    AWAKE = 'online'
