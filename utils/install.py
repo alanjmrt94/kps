c2059248f95b75e4a50b5de5f73a346f12056a40
alanjmrt94 <alanjmartinez94@gmail.com>
2023-08-28 14:46:49 -0300

    Auto install dependencies depending on OS platform and py version, refactors

--- utils/install.py
+++ utils/install.py
@@ -3,9 +3,18 @@ import subprocess
 import sys
 
 from utils.const import OsType
+from utils.version import Py_version
 
 module = ""
 venv_path = "../.venv"
+
+ubuntu_apt_reqs = "libcairo2 libcairo2-dev pkg-config python3-dev libgirepository1.0-dev python3-gi libxt-dev python3-uinput"
+pip_reqs = ["pycairo", "PyGObject", "python-uinput"]
+
+def install_apt():
+    print("\Check and install apt packages:\n")
+    proc = subprocess.Popen('sudo apt install -y' + ubuntu_apt_reqs, shell=True, executable="/bin/bash")
+    proc.wait()
 
 def create_and_activate_venv(venv_path):
     subprocess.run([sys.executable, "-m", "venv", venv_path], check=True)
@@ -19,25 +28,14 @@ def create_and_activate_venv(venv_path):
 
 def install_package(venv_path, package):
     print("\nInstalling package {0}\n".format(package))
-    subprocess.run([os.path.join(venv_path, "bin", "pip"), "install", package], check=True)
-    subprocess.run([os.path.join(venv_path, "bin", "pip3"), "install", package], check=True)
+    if (Py_version() < 3):
+        subprocess.run([os.path.join(venv_path, "bin", "pip"), "install", package], check=True)
+    else:
+        subprocess.run([os.path.join(venv_path, "bin", "pip3"), "install", package], check=True)
 
-def Install_apt():
-    print("\Check and install apt packages:\n")
-    proc = subprocess.Popen('sudo apt install -y libcairo2 libcairo2-dev pkg-config python3-dev libgirepository1.0-dev python3-gi libxt-dev python3-uinput', shell=True, executable="/bin/bash")
-    proc.wait()
-
-def Install(package_name):
+def install(package_name):
     create_and_activate_venv(venv_path)
     install_package(venv_path, package_name)
-    
-def test_package():
-    try:
-        print("\n\tTesting new package...\n")
-        # TODO: TEST FUNC
-    except:
-        print()
-        # print("\n\tPackage {} couldn't be loaded".format(module))
 
 def deactivate_env():
     # Deactivate the virtual environment
@@ -51,3 +49,18 @@ def deactivate_env():
 def Restart():
     print("restart")
     subprocess.run([os.path.join(venv_path, "bin", "python3"), "kps.py"], check=True)
+
+def Autoinstall():
+    if os.name == OsType.UNIX:
+        install_apt()
+        install("pycairo")
+        install("PyGObject")
+        install("python-uinput")
+
+def test_package():
+    try:
+        print("\n\tTesting new package...\n")
+        # TODO: TEST FUNC
+    except:
+        print()
+        # print("\n\tPackage {} couldn't be loaded".format(module))
